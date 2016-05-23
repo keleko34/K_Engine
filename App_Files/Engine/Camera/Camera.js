b7acb35acc71b06b94706c9f95f4ac461aebdef5
keleko34 <gregory.guidero@gmail.com>
2016-05-23 17:03:54 +0200

    start camera

--- App_Files/Engine/Camera/Camera.js
+++ App_Files/Engine/Camera/Camera.js
@@ -1,9 +1,22 @@
 module.exports = function(){
 
-  var freeMode = false;
+  var _freemode = false,
+      _camera = {};
 
   function Camera(){
+    _camera = new THREE.PerspectiveCamera( 45, (Engine.resolution().w / Engine.resolution().h), 1, 10000 );
+    _camera.position.z = 1000;
 
+    Engine.Input.addEnvironment('freemode')
+    .addBinding('Toggle FreeMode','keyup','p',Camera.toggleFreeMode,true);
+  }
+
+  Camera.toggleFreeMode = function(){
+    _freemode = !_freemode;
+    if(_freemode){
+       Engine.Input.environment('freemode');
+    }
+    return Camera;
   }
 
   return Camera;
